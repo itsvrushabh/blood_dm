0b30e8e4a220638921bdb3d48406e67676b3214e
itsvrushabh <vrushabh.deshmukh@infinitylabs.in>
2018-10-31 13:53:15 +0530

    first commit type

---
+++ blooddonation/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from models import City
+from models import State
+from models import BloodGroupType
+from models import Facts
+from models import NotDonate
+from models import Medication
+from models import FamousQuotes
+from models import Details
+from models import UrgentBlood
+
+admin.site.register(City)
+admin.site.register(State)
+admin.site.register(BloodGroupType)
+admin.site.register(Facts)
+admin.site.register(NotDonate)
+admin.site.register(Medication)
+admin.site.register(FamousQuotes)
+admin.site.register(Details)
+admin.site.register(UrgentBlood)
